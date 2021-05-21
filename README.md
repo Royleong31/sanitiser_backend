79b786b71ae49874ff88e0c4a8e5a0331b275935
Jing Xuan <jingxuan@Jings-MacBook-Pro.local>
2021-05-21 16:39:50 +0800

    added readme

--- README.md
+++ README.md
@@ -1,40 +1,62 @@
 # Sanitiser Backend
-- It is hosted on Firebase using Firebase Cloud Functions
 - The code for the APIs are in the functions/index.js
 - There are 2 APIs, 1 for resetting and 1 for indicating that a dispenser was used.
 - As the gateway does not check if the dispenserId and companyId are valid, all the checks are done on the backend. Both APIs will reject the request if the companyId or dispenserId are incorrect
 - More details on the specific workings of each API can be found in the comments in the code in the index.js file
+- It uses a serverless architecture using Firebase Cloud Functions, Firebase Firestore and Firebase Cloud Messaging
 
 ## Database Architecture
-- There are 3 main parties: Companies, Users, and dispensers.
+- There are 3 main groups: Companies, Users, and dispensers.
 - Each company has many dispensers
 - Each company has many users 
-- When dispenser is refilled/refill running low, notifications will be sent to the users that are registered to the same company as that dispenser
-
 
 ### Tables 
 1) usageData
-    Stores a new document every time dispensers are used or refilled
-    Stores details of the dispenser, time, and the type of usage (whether is was used or refilled) 
-    If dispenser was used, wasUsed == true, if dispenser was refilled, wasUsed == false
+    - Stores a new document every time dispensers are used or refilled
+    - Stores dispenserId (STRING), companyId (STRING), timeStamp (STRING) and wasUsed (BOOL)
+    - If dispenser was used, wasUsed == true, if dispenser was refilled, wasUsed == false
+    - timeStamp is stored in ISO format
     
 2) users
+    - Stores 1 document per user
+    - Stores companyId (STRING), email (STRING), name (STRING), notificationLevel (NUMBER), notifyWhenRefilled (BOOL), deviceTokens (STRING[]) and userId (STRING)
+    - notifyWhenRefiled, notificationLevel determine when the notifications will be sent
+    - deviceTokens store an array of the devices that the user is logged in to. Relevant notifications will be sent to the devices in the array.
     
 3) dispensers
+    - Stores companyId (STRING), dispenserId (STRING), level (NUMBER), limit (NUMBER), location (STRING), useCount (NUMBER), and userId (STRING)
+    - Whenever dispenser is used, the level will decrease (level represents the refill level in percentage)
+    - Limit is the total number of times the dispenser can be used (default is 10)
+    - useCount is the number of times the dispenser has been used since the last refill.
+    - level = useCount/limit * 100
+    
 4) companies
+    - Stores companyName (STRING), dispensers (STRING[]), users (STRING[])
+    - companyId, which is used in other collections, refers to the document ID of the company document
+    - dispensers stores an array of dispenser IDs of the dispensers that are registered with the company
+    - users stores the user IDs of the users that are registered with the company
+    
+### Registration
+- In order to register a company, the admin of the system needs to manually create a document in the company collection. This is to ensure that users do not have the ability to edit the details of the company collection directly
+- Registration of dispensr is done through the app, whereby users can key in the location of the new dispenser and key in the dispenserId of the dispenser
+- Each dispenser will ideally have a QR code which can be scanned with the app in order to register the dispenser.
+- The QR code contains the dispenserId of the dispenser.
+- During registration, the dispenserId will be added to the company's dispensers array
 
 ### Notifications
 - Notifications are sent to relevant users only when they are currently logged in
 - The device tokens of the logged in users are stored in the user document, and Firebase Cloud Messaging is used to send notifications to these users.
-- 2 types of notifications: 
+- 3 types of notifications: 
     1) Notify when refilled
     2) Notify when refill is running low (user can choose the refill level at which they want to be notified, from 5% -50%)
+    3) Notify when refill is empty (refill level == 0)
 
 ## Usage API
 - The gateway/dispenser with WiFi will send API with the companyId, dispenserId and typeOfUsage.
-- The database will be updated by adding a document to usageData and updating the dispenserLevel in the dispenser document
+- The database will be updated by adding a document to usageData and updating the dispenserLevel and useCount in the dispenser document
 - The dispenserLevel will be checked against the refill level that the users in the company chose to receive notifications at (notificationLevel variable in user document)
     - If the dispenserLevel is below the notificationLevel, a notification will be sent to mobile apps that are currently logged into the same company that the dispenser belongs to.
+    - Notification will also be sent when the refill runs out
 
 ## Reset API
 - The database will be updated by adding a document to usageData and updating the dispenserLevel in the dispenser document
--- functions/index.js
+++ functions/index.js
@@ -26,6 +26,7 @@ app.patch("/:companyId/:dispenserId/usage", async (req, res) => {
     .where("companyId", "==", companyId) // change to companyId == companyId
     .get();
 
+//  dispenserInfo consists of only 1 element, but I used a forEach loop as it didnt work without
   dispenserInfo.forEach((doc) => {
     dispenserDocId = doc.id;
     dispenserData = doc.data();
@@ -34,14 +35,15 @@ app.patch("/:companyId/:dispenserId/usage", async (req, res) => {
   console.log(`DispenserDocId : ${dispenserDocId}`);
   console.log(`DispenserData : ${dispenserData}`);
 
+//    Reject the API request and send an error to the sender if there was no dispenser with the corresponding dispenserId and companyId
   if (!dispenserData) {
     res.status(400).send("Invalid user ID or dispenser ID");
     return;
   }
 
-  let companyInfo = await db.collection('companies').doc(companyId).get();
+  let companyInfo = await db.collection('companies').doc(companyId).get(); // company document
   let companyData = companyInfo.data();
-  let users = companyData['users'];
+  let users = companyData['users']; // array of users in the company
 
   let userData = [];
   let userInfo = await db
@@ -49,18 +51,19 @@ app.patch("/:companyId/:dispenserId/usage", async (req, res) => {
     .where("companyId", "==", companyId)
     .get();
 
-  userInfo.forEach(doc => userData.push(doc.data()));
+  userInfo.forEach(doc => userData.push(doc.data())); // contains the document details about each user
   userData.forEach((doc) => { 
     console.log(`User notification Level: ${doc['notificationLevel']}`);
   });
 
 
-  const limit = dispenserData["limit"];
-  const location = dispenserData["location"];
-  let newUseCount = 1 + dispenserData["useCount"];
+  const limit = dispenserData["limit"]; // capacity of the dispenser
+  const location = dispenserData["location"]; // location name of the dispenser
+  let newUseCount = 1 + dispenserData["useCount"]; // newUseCount = old use count + 1
   // newUseCount = newUseCount >= limit ? limit : newUseCount;
 
-  if (newUseCount <= limit) {
+  if (newUseCount <= limit) { // if the refill is not yet empty
+//      Add a new document into the usageData collection
     db.collection("usageData").add({
       dispenserId,
       timeStamp: new Date().toISOString(),
@@ -68,25 +71,28 @@ app.patch("/:companyId/:dispenserId/usage", async (req, res) => {
       wasUsed: true,
     });
 
+//      New refill level of the dispenser
     let newLevel = 100 - Math.round(100 * (newUseCount / limit)); //if newUseCount == 100, newLevel = 0
 
+//      Update the dispenser document in the dispensers collection
     await db.collection("dispensers").doc(dispenserDocId).update({
       useCount: newUseCount,
       level: newLevel,
     });
 
-    for (let user of userData) {
+    for (let user of userData) { // for each user that is registered to this company
       const userDeviceList = user['deviceTokens'];
       const notificationLevel = +user['notificationLevel'];
       console.log(`USER: DEVICE LIST: ${userDeviceList}`);
       console.log(`USER: NOTIFICATION LEVEL: ${notificationLevel}`);
       
-      if (
+      if ( // if the refill is empty or is below the preset refill level threshold
         newUseCount == limit ||
         newUseCount == Math.ceil(limit - notificationLevel / 100 * limit) // send notification at 1 below the threshold
       ) {
         console.log(`Triggering cloud messaging for usage as newUseCount = ${newUseCount}`);
 
+//          bodyText and payload are both parts of the message that is sent to the devices to produce an app notification
         let bodyText =
           newLevel == 0
             ? `${location} refill is empty`
@@ -144,6 +150,7 @@ app.patch("/:companyId/:dispenserId/usage", async (req, res) => {
 
 // Resetting API
 app.patch("/:companyId/:dispenserId/reset", async (req, res) => {
+//    much of the code is similar to the above code
   const companyId = req.params.companyId;
   const dispenserId = req.params.dispenserId;
   let dispenserData, dispenserDocId;
@@ -186,23 +193,23 @@ app.patch("/:companyId/:dispenserId/reset", async (req, res) => {
   const level = dispenserData["level"];
   const location = dispenserData["location"];
 
-  if (+level === 100) {
+  if (+level === 100) { // check if the refill is already full
     res.send("Refill is already full");
   } else {
 
-    await db.collection("dispensers").doc(dispenserDocId).update({
+    await db.collection("dispensers").doc(dispenserDocId).update({ // update the dispenser document with the new refill level
       useCount: 0,
       level: 100,
     });
 
-    db.collection("usageData").add({
+    db.collection("usageData").add({ // add a new usageData document
       dispenserId,
       timeStamp: new Date().toISOString(),
       companyId,
       wasUsed: false,
     });
 
-    for (let user of userData) {
+    for (let user of userData) { // for each user that is registered to this company
       const userDeviceList = user['deviceTokens'];
       const notifyWhenRefilled = user['notifyWhenRefilled'];
       console.log(`USER: DEVICE LIST: ${userDeviceList}`);
@@ -219,7 +226,7 @@ app.patch("/:companyId/:dispenserId/reset", async (req, res) => {
         },
       };
 
-      if (notifyWhenRefilled) {
+      if (notifyWhenRefilled) { // send notification to them if they want to be notified
         try {
           console.log("sending reset notification");
           const response = await admin
